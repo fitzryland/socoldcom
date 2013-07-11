5cf70457d74d6339061118601109ccf33e345790
Fitz Ryland <fitzryland@gmail.com>
2013-07-10 19:40:11 -0700

    basic home page in

---
+++ wp-content/themes/socold/home-template.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area tansp">
+		<div id="content" class="site-content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<div class="cocktails">
+						<h2><?php the_field('c_title'); ?></h2>
+						<?php echo custom_acf_image_output(get_field('c_product_photo'), '', ''); ?>
+						<div class="sellingPoints">
+							<?php the_field('c_selling_points'); ?>
+						</div>
+						<a href="<?php the_field('c_area_link'); ?>" class="button">More</a>
+					</div>
+					<div class="shavedice">
+						<h2><?php the_field('si_title'); ?></h2>
+						<?php echo custom_acf_image_output(get_field('si_product_photo'), '', ''); ?>
+						<div class="sellingPoints">
+							<?php the_field('si_selling_points'); ?>
+						</div>
+						<a href="<?php the_field('si_area_link'); ?>" class="button">More</a>
+					</div>
+
+				</article><!-- #post-## -->
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
